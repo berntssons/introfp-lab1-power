9a3449bec0a91c5a1d95167c6fabce58796be6dd
sanna <susanna.berntsson2000@gmail.com>
2025-09-03 09:46:53 +0000

    add solutions to part A - D (wip)

--- Lab1.hs
+++ Lab1.hs
@@ -30,44 +30,70 @@ power n k = n * power n (k-1)
 -- => { definition of power, third case }
 -- 3 * power 3 (2 - 1)
 -- => { apply subtraction }
---  ...
--- => { ... }
--- ...
--- => { ... }
--- ...
--- => { ... }
--- ...
--- => { ... }
--- ...
--- => { ... }
+--  3 * power 3 1
+-- => { definition of power, third case }
+-- 3 * 3 * power 3 (1 - 1)
+-- => { apply subtraction }
+-- 3 * 3 * power 3 0
+-- => { definition of power, second case }
+-- 3 * 3 * 1
+-- => { apply multiplication }
+-- 9 * 1
+-- => { apply multiplication }
 -- 9
 
 -- Part B ----------------------------------------------------------------------
 
-power1 = undefined
+power1 :: Int -> Int -> Int
 
+-- Case for negative exponents:
+power1 n k
+  | k < 0 = error "power: negative argument"
+-- Case for k >= 0:
+power1 n k = product (replicate k n)
+-- Separate case for k = 0 not needed since product [] returns 1
 
 -- Part C ----------------------------------------------------------------------
 
-power2 = undefined
+power2 :: Int -> Int -> Int
 
+-- Using prelude functions
+-- power2 n k = if even k then (n * n)^(k `div` 2) else n * (n^(k - 1))
+
+-- Recursive
+power2 n k
+  | k < 0 = error "power: negative argument"
+power2 n 0 = 1
+power2 n k = if even k then power2 (n * n) (k `div` 2) else n * power2 n (k - 1)
 
 -- Part D ----------------------------------------------------------------------
 
 -- Part D.1 - describe your test cases here
 
-test1 = power1 2 3 == power 2 3
-test2 = undefined -- replace with your own test case
-test3 = undefined -- replace with your own test case
-test4 = undefined -- replace with your own test case
-test5 = undefined -- replace with your own test case
+hasSameResult :: (Int -> Int -> Int) -> (Int -> Int -> Int) -> Int -> Int -> Bool
+hasSameResult f g n k = f n k == g n k
+
+matchesPower :: (Int -> Int -> Int) -> Int -> Int -> Bool
+matchesPower f n k = hasSameResult power f n k
+
+testBothPowers :: Int -> Int -> Bool
+testBothPowers n k = matchesPower power1 n k && matchesPower power2 n k
+
+test1 = testBothPowers 2 4  -- Test positive even exponent
+test2 = testBothPowers 3 3 -- Test positive odd exponent
+test3 = testBothPowers 4 0 -- Test exponent is 0
+test4 = testBothPowers (-2) 3 -- Test negative base and odd exponent
+
+-- Test negative exponent throws error
+test5 = power1 4 (-3)
+test6 = power2 4 (-3)
 
 -- Part D.2
 comparePower1 :: Int -> Int -> Bool
-comparePower1 n k = undefined
+comparePower1 n k = matchesPower power1 n k
 
 comparePower2 :: Int -> Int -> Bool
-comparePower2 n k = undefined
+comparePower2 n k = matchesPower power2 n k
 
 -- Part D.3
 testAll :: Bool
