bef3fa0f877ab956cd3ac31323b6e6bfbe6b4eca
Eeric <eeric.brask@gmail.com>
2025-09-04 14:36:27 +0000

    solution d finish

--- Lab1.hs
+++ Lab1.hs
@@ -6,11 +6,13 @@ License     : BSD
 Maintainer  : alexg@chalmers.se
 Stability   : experimental
 
-Authors     : <list your names here>
-Lab group   : <group number>
+Authors     : Susanna Berntsson, Eeric Brask, Max Jenslöv
+Lab group   : 70
 -}
 
 module Lab1 where
+
+import MeasureTime
 
 -- The power function uses explicit recursion to calculate n^k. We developed
 -- this function during a lecture.
@@ -57,44 +59,46 @@ power1 n k = product (replicate k n)
 
 power2 :: Int -> Int -> Int
 
--- Using prelude functions
--- power2 n k = if even k then (n * n)^(k `div` 2) else n * (n^(k - 1))
-
--- Recursive
 power2 n k
   | k < 0 = error "power: negative argument"
 power2 n 0 = 1
-power2 n k = if even k then power2 (n * n) (k `div` 2) else n * power2 n (k - 1)
+power2 n k
+  | even k = power2 (n * n) (k `div` 2)
+  | otherwise = n * power2 n (k - 1)
 
 -- Part D ----------------------------------------------------------------------
 
 -- Part D.1 - describe your test cases here
 
-hasSameResult :: (Int -> Int -> Int) -> (Int -> Int -> Int) -> Int -> Int -> Bool
-hasSameResult f g n k = f n k == g n k
+-- Test positive odd exponent
+test1 = power 2 3 == power1 2 3 && power 2 3 == power2 2 3
 
-matchesPower :: (Int -> Int -> Int) -> Int -> Int -> Bool
-matchesPower f n k = hasSameResult power f n k
+-- Test positive even exponent
+test2 = power 3 4 == power1 3 4 && power 3 4 == power2 3 4
 
-testBothPowers :: Int -> Int -> Bool
-testBothPowers n k = matchesPower power1 n k && matchesPower power2 n k
+-- Test exponent is 0
+test3 = power 3 0 == power1 3 0 && power 3 0 == power2 3 0
 
-test1 = testBothPowers 2 4  -- Test positive even exponent
-test2 = testBothPowers 3 3 -- Test positive odd exponent
-test3 = testBothPowers 4 0 -- Test exponent is 0
-test4 = testBothPowers (-2) 3 -- Test negative base and odd exponent
+-- Test base is 0
+test4 = power 0 5 == power1 0 5 && power 0 5 == power2 0 5
 
--- Test negative exponent throws error
-test5 = power1 4 (-3)
-test6 = power2 4 (-3)
+-- Test negative base
+test5 = power (-5) 3 == power1 (-5) 3 && power (-5) 3 == power2 (-5) 3
 
 -- Part D.2
+
 comparePower1 :: Int -> Int -> Bool
-comparePower1 n k = matchesPower power1 n k
+comparePower1 n k = power n k == power1 n k
 
 comparePower2 :: Int -> Int -> Bool
-comparePower2 n k = matchesPower power2 n k
+comparePower2 n k = power n k == power2 n k
 
 -- Part D.3
+testValues :: [(Int, Int)]
+testValues = [(2, 3), (3, 4), (3, 0), (0, 5), ((-5), 3)]
+
 testAll :: Bool
-testAll = undefined
+testAll = and [ comparePower1 n k && comparePower2 n k | x <- testValues, let n = fst x, let k = snd x ]
+
+-- Part E
+
